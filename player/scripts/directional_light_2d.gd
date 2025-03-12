5c3e71b96b2586d5d13b99122a13a9720fc0e895
AyaPK <Sherbnya@gmail.com>
2025-03-12 20:20:42 +0000

    Add basic light detection

---
+++ player/scripts/directional_light_2d.gd
@@ -0,0 +1,41 @@
+class_name LightDetector extends Node2D
+
+@onready var player: CharacterBody2D = $".."
+
+@export var line_length: float = 1000.0
+@export var light_angle: float = 75.0
+@export var side_offset: float = 40.0 
+
+var hit_by_light: bool = true
+
+func _process(delta):
+	queue_redraw()
+
+func _draw():
+	if player:
+		var start_position = player.global_position
+		var angle_radians = deg_to_rad(light_angle)
+
+		var direction = Vector2(cos(angle_radians), -sin(angle_radians))
+
+		var perpendicular = direction.orthogonal()
+
+		var start_positions = [
+			start_position,
+			start_position - Vector2(side_offset, 0),
+			start_position + Vector2(side_offset/2, 0)
+		]
+
+		var space_state = get_world_2d().direct_space_state
+		var count: int = 0
+		for start in start_positions:
+			var end_position = start + direction * line_length
+			var query = PhysicsRayQueryParameters2D.create(start, end_position)
+			var result = space_state.intersect_ray(query)
+			
+			if result:
+				count += 1
+			if count != 3:
+				hit_by_light = true
+			else:
+				hit_by_light = false
--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -1,18 +1,17 @@
-extends CharacterBody2D
+class_name Player extends CharacterBody2D
 
-@export var speed: float = 250.0  # Max movement speed
-@export var acceleration: float = 2500.0  # Speeding up rate
-@export var friction: float = 2500.0  # Slowing down rate
-@export var jump_velocity: float = -420.0  # Jumping strength
-@export var gravity: float = 1200.0  # Gravity force
-@export var air_control: float = 0.6  # Movement control in air (0.0 - 1.0)
+@export var speed: float = 250.0
+@export var acceleration: float = 2500.0
+@export var friction: float = 2500.0
+@export var jump_velocity: float = -420.0
+@export var gravity: float = 1200.0
+@export var air_control: float = 0.6 
+@onready var light_detector: LightDetector = $LightDetector
 
 func _physics_process(delta: float) -> void:
-	# Apply gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle movement input
 	var direction: float = Input.get_axis("ui_left", "ui_right")
 	var target_speed = direction * speed
 
@@ -26,9 +25,12 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, decel * delta)
 		$AnimationPlayer.play("idle")
 
-	# Jumping
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = jump_velocity
 
-	# Apply movement
 	move_and_slide()
+	
+	if light_detector.hit_by_light:
+		modulate = Color(0.25, 0, 0)
+	else:
+		modulate = Color(1, 1, 1)
