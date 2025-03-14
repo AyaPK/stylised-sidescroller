1ce3a4c1dcf60dbd5c0c9361a320d73ed85e5f0a
AyaPK <Sherbnya@gmail.com>
2025-03-14 11:56:25 +0000

    Jump and fall

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -6,9 +6,14 @@ class_name Player extends CharacterBody2D
 @export var jump_velocity: float = -420.0
 @export var gravity: float = 1200.0
 @export var air_control: float = 0.6 
+
 @onready var light_detector: LightDetector = $LightDetector
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Icon
+@onready var jump_sound: AudioStreamPlayer = $JumpSound
+@onready var land_sound: AudioStreamPlayer = $LandSound
+
+var was_in_air: bool = false
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -22,26 +27,30 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, target_speed, accel * delta)
 		sprite.flip_h = direction < 0 
 		
-		# Only play walk animation if on the floor
 		if is_on_floor():
 			animation_player.play("walk")
 	else:
 		var decel = friction if is_on_floor() else friction * air_control
 		velocity.x = move_toward(velocity.x, 0, decel * delta)
 
-		# Only play idle if on the floor
 		if is_on_floor():
 			animation_player.play("idle")
 
+	# Jumping
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 		animation_player.play("jump")
+		jump_sound.play()
+		was_in_air = true
 
 	move_and_slide()
 
-	# Handle fall animation
 	if velocity.y > 0 and not is_on_floor():
 		animation_player.play("fall")
+
+	if is_on_floor() and was_in_air:
+		land_sound.play()
+		was_in_air = false
 
 	if light_detector.hit_by_light:
 		Ui.remove_health()
